f371f1b821c08ff21625c63a430b9e8c6868277a
将 <2710870161@qq.com>
2025-01-07 03:48:47 +0800

    fix: update vercel config and service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,1 +1,12 @@
-importScripts("https://js.pusher.com/beams/service-worker.js"); 
+// 基础的 Service Worker
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(fetch(event.request));
+}); 
