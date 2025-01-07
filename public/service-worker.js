34d13839e373f90bf3c3a36a0e2b3c06d37e4336
将 <2710870161@qq.com>
2025-01-07 18:55:11 +0800

    “域名消息测试2”

--- public/service-worker.js
+++ public/service-worker.js
@@ -8,5 +8,13 @@ self.addEventListener('activate', (event) => {
 });
 
 self.addEventListener('fetch', (event) => {
+  // 处理 %PUBLIC_URL% 的替换
+  const url = event.request.url;
+  if (url.includes('%PUBLIC_URL%')) {
+    const newUrl = url.replace(/%PUBLIC_URL%\//g, '');
+    event.respondWith(fetch(newUrl));
+    return;
+  }
+  
   event.respondWith(fetch(event.request));
 }); 
