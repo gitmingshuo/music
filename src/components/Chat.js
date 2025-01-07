e158b4f8f9a39a6bc84e670b99aa6c80c13a60e1
将 <2710870161@qq.com>
2025-01-07 19:16:02 +0800

    “域名消息测试3”

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,10 +1,16 @@
 import React, { useState, useEffect } from 'react';
+import Pusher from 'pusher-js';
+import { API_BASE_URL } from '../config/api';
 
 function Chat() {
   const [messages, setMessages] = useState([]);
   
   useEffect(() => {
-    // 假设您已经初始化了 Pusher
+    const pusher = new Pusher('4b522f1169d2c59a5253', {
+      cluster: 'ap1',
+      encrypted: true
+    });
+    
     const channel = pusher.subscribe('chat-channel');
     
     channel.bind('new-message', (data) => {
